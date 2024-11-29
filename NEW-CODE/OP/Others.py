85108c5af81d1235f8946d18bd174a861494b3a3
NinaZUO <ninazuo@smail.nju.edu.cn>
2024-11-29 17:37:55 +0800

    Update Others.py

--- NEW-CODE/OP/Others.py
+++ NEW-CODE/OP/Others.py
@@ -1,73 +1,80 @@
 import torch
-OPclass_name_others=['OP_Closure','OP_Basic']
+
+OPclass_name_others = ['OP_Closure', 'OP_Basic']
+
+
 class OP_Closure:
     def __init__(self):
-        self.func_list = ["id_industry","id_int","id_float"]
+        self.func_list = ["id_industry", "id_int", "id_float"]
+
     @staticmethod
     def id_industry(industry):
         return industry
+
     @staticmethod
     def id_int(int):
         return int
+
     @staticmethod
     def id_float(thresh):
         return thresh
-    
+
+
 class OP_Basic:
     def __init__(self):
-        self.func_list = ["nanmean","nanstd","corrwith","rank_corrwith","multi_regress","regress"]
+        self.func_list = ["nanmean", "nanstd", "corrwith", "rank_corrwith", "multi_regress", "regress"]
 
     @staticmethod
-    def nanmean(self,tensor,dim=-1):
+    def nanmean(tensor, dim=-1):
         return torch.nansum(tensor, dim=dim) / torch.sum(~torch.isnan(tensor), dim=dim)
 
     @staticmethod
-    def nanstd(self,x,dim=-1):
-        mean = self.nanmean(x, dim=dim)
+    def nanstd(x, dim=-1):
+        mean = OP_Basic.nanmean(x, dim=dim)
         centered_tensor = (x - mean.unsqueeze(dim))
         centered_tensor = centered_tensor.masked_fill(torch.isnan(centered_tensor), 0)
         var = torch.nansum(centered_tensor.pow(2), dim=dim)
         n = torch.sum(~torch.isnan(x), dim=dim)
-        return torch.sqrt(var / n-1)
+        return torch.sqrt(var / n - 1)
 
     @staticmethod
-    def corrwith(self,tensor1,tensor2,dim=-1):
+    def corrwith(tensor1, tensor2, dim=-1):
         mask = ~(torch.isnan(tensor1) | torch.isnan(tensor2))
         tensor1 = torch.where(mask, tensor1, float('nan'))
         tensor2 = torch.where(mask, tensor2, float('nan'))
 
-        tensor1_mean = self.nanmean(tensor1, dim=dim)
-        tensor2_mean = self.nanmean(tensor2, dim=dim)
-        tensor1_std = self.nanstd(tensor1, dim=dim, unbiased=False)
-        tensor2_std = self.nanstd(tensor2, dim=dim, unbiased=False)
+        tensor1_mean = OP_Basic.nanmean(tensor1, dim=dim)
+        tensor2_mean = OP_Basic.nanmean(tensor2, dim=dim)
+        tensor1_std = OP_Basic.nanstd(tensor1, dim=dim, unbiased=False)
+        tensor2_std = OP_Basic.nanstd(tensor2, dim=dim, unbiased=False)
         centered_tensor1 = tensor1 - tensor1_mean.unsqueeze(dim)
         centered_tensor2 = tensor2 - tensor2_mean.unsqueeze(dim)
-        covariance = self.nanmean(centered_tensor1*centered_tensor2, dim=dim)
+        covariance = OP_Basic.nanmean(centered_tensor1 * centered_tensor2, dim=dim)
         correlation = covariance / (tensor1_std * tensor2_std)
         return correlation
 
     @staticmethod
-    def rank_corrwith(self,tensor1,tensor2,dim=-1):
+    def rank_corrwith(tensor1, tensor2, dim=-1):
         nan_mask = torch.isnan(tensor1) | torch.isnan(tensor2)
         tensor1 = tensor1.masked_fill(nan_mask, float('nan'))
-        tensor1 = torch.argsort(torch.argsort(tensor1,dim=dim))
-        tensor1=torch.where(~nan_mask,tensor1,float('nan'))
+        tensor1 = torch.argsort(torch.argsort(tensor1, dim=dim))
+        tensor1 = torch.where(~nan_mask, tensor1, float('nan'))
         tensor2 = tensor2.masked_fill(nan_mask, float('nan'))
-        tensor2 = torch.argsort(torch.argsort(tensor2,dim=dim))
+        tensor2 = torch.argsort(torch.argsort(tensor2, dim=dim))
         tensor2 = torch.where(~nan_mask, tensor2, float('nan'))
 
-        tensor1_mean = self.nanmean(tensor1, dim=dim)
-        tensor2_mean = self.nanmean(tensor2, dim=dim)
-        tensor1_std = self.nanstd(tensor1, dim=dim, unbiased=False)
-        tensor2_std = self.nanstd(tensor2, dim=dim, unbiased=False)
+        tensor1_mean = OP_Basic.nanmean(tensor1, dim=dim)
+        tensor2_mean = OP_Basic.nanmean(tensor2, dim=dim)
+        tensor1_std = OP_Basic.nanstd(tensor1, dim=dim, unbiased=False)
+        tensor2_std = OP_Basic.nanstd(tensor2, dim=dim, unbiased=False)
         centered_tensor1 = tensor1 - tensor1_mean.unsqueeze(dim)
         centered_tensor2 = tensor2 - tensor2_mean.unsqueeze(dim)
-        covariance = self.nanmean(centered_tensor1*centered_tensor2, dim=dim)
+        covariance = OP_Basic.nanmean(centered_tensor1 * centered_tensor2, dim=dim)
         correlation = covariance / (tensor1_std * tensor2_std)
         return correlation
 
     @staticmethod
-    def multi_regress(self,y, x_s, dim=-1):
+    def multi_regress(y, x_s, dim=-1):
         down_epsilon = 1e-10
         up_epsilon = 1e20
 
@@ -95,5 +102,5 @@ class OP_Basic:
         return k, b, res
 
     @staticmethod
-    def regress(self,y, x_s, dim=-1):
-        return self.multi_regress(y, x_s.unsqueeze(-1), dim=dim)
+    def regress(y, x_s, dim=-1):
+        return OP_Basic.multi_regress(y, x_s.unsqueeze(-1), dim=dim)
