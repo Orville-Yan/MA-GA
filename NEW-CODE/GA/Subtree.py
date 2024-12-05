59a51d745014cf4e15eac21b9f23e2db766f64e7
NinaZUO <ninazuo@smail.nju.edu.cn>
2024-12-05 21:07:17 +0800

    add BBD2A BB2A

--- NEW-CODE/GA/Subtree.py
+++ NEW-CODE/GA/Subtree.py
@@ -1,11 +1,14 @@
 from GA_tools import *
-from OP import *
+from OP.ToA import *
+from OP.Others import *
 import numpy as np
 from deap import gp, base, tools, creator
 class Subtree:
-    def __init__(self, OP_BD2A_func_list: list, OP_B2A_func_list: list, population_size=10):
-        self.OP_BD2A_func_list = OP_BD2A_func_list
-        self.OP_B2A_func_list = OP_B2A_func_list
+    def __init__(self,population_size=10):
+        self.OP_BD2A_func_list = ['D_Minute_area_mean', 'D_Minute_area_std', 'D_Minute_area_sum', 'D_Minute_area_prod']
+        self.OP_B2A_func_list = ['D_Minute_std', 'D_Minute_mean', 'D_Minute_trend']
+        self.OP_BBD2A_func_list = ['D_Minute_area_weight_mean', 'D_Minute_area_corr', 'D_Minute_area_rankcorr','D_Minute_area_bifurcate_mean', 'D_Minute_area_bifurcate_std']
+        self.OP_BB2A_func_list = ['D_Minute_corr', 'D_Minute_weight_mean']
         self.population_size = population_size
 
     def generate_toolbox_with_mask(self):
@@ -14,6 +17,10 @@ class Subtree:
         for func_name in self.OP_BD2A_func_list:
             func = getattr(OP_BD2A, func_name)
             self.pset_with_mask.addPrimitive(func, [TypeB, TypeD], TypeA, name=func_name)
+
+        for func_name in self.OP_BBD2A_func_list:
+            func = getattr(OP_BBD2A, func_name)
+            self.pset_with_mask.addPrimitive(func, [TypeB, TypeB, TypeD], TypeA, name=func_name)
 
         self.pset_with_mask.addTerminal(np.random.rand(1)[0], TypeA)
 
@@ -32,6 +39,10 @@ class Subtree:
         for func_name in self.OP_B2A_func_list:
             func = getattr(OP_B2A, func_name)
             self.pset_no_mask.addPrimitive(func, [TypeB], TypeA, name=func_name)
+
+        for func_name in self.OP_BB2A_func_list:
+            func = getattr(OP_BB2A, func_name)
+            self.pset_no_mask.addPrimitive(func, [TypeB, TypeB], TypeA, name=func_name)
 
         self.pset_no_mask.addTerminal(np.random.rand(1)[0], TypeA)
 
