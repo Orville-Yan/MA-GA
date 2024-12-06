b3c9bae0055ccf4360670a6037d72d85e351adbb
CrystalShrimp <shuman0805@163.com>
2024-12-06 15:36:29 +0800

    Update Branch.py

--- NEW-CODE/GA/Branch.py
+++ NEW-CODE/GA/Branch.py
@@ -6,28 +6,32 @@ from OP import *
 from GA_tools import *
 
 class Branch:
-    def __init__(self, mp_root: 'MP_Root', population_size: int, OP_func_list: list, terminal_type: type):
+    def __init__(self, mp_root: 'MP_Root', population_size: int,OP_func,OP_func_list:list,OP_input_type,OP_output_type):
         self.input = mp_root
         self.population_size = population_size
+        self.OP_func = OP_func
         self.OP_func_list = OP_func_list
-        self.terminal_type = terminal_type
+        self.OP_input_type = OP_input_type
+        self.OP_output_type = OP_output_type
+        self.pset = None
+        self.toolbox = None
+        self.int_values = [torch.tensor(i,dtype=torch.int) for i in [2, 3, 5, 8, 10 ,30]]   
 
     def generate_toolbox(self):
-        self.pset = gp.PrimitiveSetTyped("MAIN", [TypeB]*len(self.input), TypeD)
-
+        class_name = self.__class__.__name__ 
+        self.pset = gp.PrimitiveSetTyped(class_name, self.OP_input_type*len(self.input), self.OP_output_type)
         for name in self.OP_func_list:
-            func = getattr(OP_B2D, name) 
-            self.pset.addPrimitive(func, TypeB, TypeD)
-
-        self.pset.addTerminal(np.random.rand(1)[0], self.terminal_type)
-
+            func = getattr(self.OP_func,name)
+            self.pset.addPrimitive(func, self.OP_input_type, self.OP_output_type)
+        for input_type in  set(self.OP_input_type):
+            for constant in self.int_values:
+                self.pset.addTerminal(constant, input_type) 
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create(self.__class__.__name__, gp.PrimitiveTree, fitness=creator.FitnessMax, pset=self.pset)
-
+        creator.create(class_name, gp.PrimitiveTree, fitness=creator.FitnessMax, pset=self.pset)
         self.toolbox = base.Toolbox()
         self.toolbox.register("expr", gp.genHalfAndHalf, pset=self.pset, min_=1, max_=1)  
-        self.toolbox.register(self.__class__.__name__, tools.initIterate, creator.__name__, self.toolbox.expr)
-        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.__name__)
+        self.toolbox.register(class_name, tools.initIterate, creator.class_name, self.toolbox.expr)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.class__name)
         self.toolbox.register("compile", gp.compile, pset=self.pset)
 
     def generate_population(self):
@@ -50,7 +54,8 @@ class M_Branch_MP2D(Branch):
             'Mmask_morning',
             'Mmask_afternoon',
         ]
-        super().__init__(mp_root, population_size, OP_B2D_func_list, TypeB) 
+        super().__init__(mp_root, population_size, OP_B2D, OP_B2D_func_list, TypeB,TypeD)
+
 
 class M_Branch_MV2D(Branch):
     def __init__(self, mp_root: 'MP_Root', population_size: int):
@@ -67,7 +72,8 @@ class M_Branch_MV2D(Branch):
             'Mmask_morning',
             'Mmask_afternoon',
         ]
-        super().__init__(mp_root, population_size, OP_B2D_func_list, TypeB)
+        super().__init__(mp_root, population_size, OP_B2D, OP_B2D_func_list, TypeB,TypeD)
+
 
 class M_Branch_MPDP2D(Branch):
     def __init__(self, mp_root: 'MP_Root', population_size: int):
@@ -75,55 +81,15 @@ class M_Branch_MPDP2D(Branch):
             'Mmask_day_plus',
             'Mmask_day_sub'
         ]
-        super().__init__(mp_root, population_size, OP_BA2D_func_list, TypeB)
+        super().__init__(mp_root, population_size, OP_BA2D,OP_BA2D_func_list,[TypeB,TypeA],TypeD)
 
-    def generate_toolbox(self):
-        self.pset = gp.PrimitiveSetTyped("MAIN", [TypeB, TypeA], TypeD)
-        for name in self.OP_BA2D_func_list:
-            func = getattr(OP_BA2D, name) 
-            self.pset.addPrimitive(func, [TypeB, TypeA], TypeD)
-        
-        self.pset.addTerminal(np.random.rand(1)[0], TypeA) 
-        self.pset.addTerminal(np.random.rand(1)[0], TypeB) 
 
-        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create("M_Branch_MPDP2D", gp.PrimitiveTree, fitness=creator.FitnessMax, pset=self.pset)
 
-        self.toolbox = base.Toolbox()
-        self.toolbox.register("expr", gp.genHalfAndHalf, pset=self.pset, min_=1, max_=1)  
-        self.toolbox.register("M_Branch_MPDP2D", tools.initIterate, creator.M_Branch_MPDP2D, self.toolbox.expr)
-        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.M_Branch_MPDP2D)
-        self.toolbox.register("compile", gp.compile, pset=self.pset)
 
-    def generate_population(self):
-        self.individuals_code = self.toolbox.population(n=self.population_size)
-        self.individuals_code, self.individuals_str = change_name(self.individuals_code, self.input)
 class M_Branch_MVDV2D(Branch):
     def __init__(self, mp_root: 'MP_Root', population_size: int):
         OP_BA2D_func_list = [
             'Mmask_day_plus',
             'Mmask_day_sub'
         ]
-        super().__init__(mp_root, population_size, OP_BA2D_func_list, TypeB)
-
-    def generate_toolbox(self):
-        self.pset = gp.PrimitiveSetTyped("MAIN", [TypeB, TypeA], TypeD)
-        for name in self.OP_BA2D_func_list:
-            func = getattr(OP_BA2D, name) 
-            self.pset.addPrimitive(func, [TypeB, TypeA], TypeD)
-        
-        self.pset.addTerminal(np.random.rand(1)[0], TypeA) 
-        self.pset.addTerminal(np.random.rand(1)[0], TypeB) 
-
-        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create("M_Branch_MVDV2D", gp.PrimitiveTree, fitness=creator.FitnessMax, pset=self.pset)
-
-        self.toolbox = base.Toolbox()
-        self.toolbox.register("expr", gp.genHalfAndHalf, pset=self.pset, min_=1, max_=1)  
-        self.toolbox.register("M_Branch_MVDV2D", tools.initIterate, creator.M_Branch_MVDV2D, self.toolbox.expr)
-        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.M_Branch_MVDV2D)
-        self.toolbox.register("compile", gp.compile, pset=self.pset)
-
-    def generate_population(self):
-        self.individuals_code = self.toolbox.population(n=self.population_size)
-        self.individuals_code, self.individuals_str = change_name(self.individuals_code, self.input)
+        super().__init__(mp_root, population_size, OP_BA2D,OP_BA2D_func_list,[TypeB,TypeA],TypeD)
