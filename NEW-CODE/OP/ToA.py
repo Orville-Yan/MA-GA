d829f557ce9926ee3cc4db7311970f706f0e4765
Orville-Yan <13396864550@163.com>
2024-12-09 17:09:17 +0800

    Update ToA.py

--- NEW-CODE/OP/ToA.py
+++ NEW-CODE/OP/ToA.py
@@ -1,5 +1,5 @@
 import torch
-from Others import OP_Basic
+from OP.Others import OP_Basic
 OPclass_name_2A = ['OP_A2A', 'OP_AE2A', 'OP_AA2A', 'OP_AG2A',
                    'OP_AAF2A', 'OP_AF2A','OP_AC2A', 'OP_BD2A', 'OP_BBD2A', 'OP_BB2A', 'OP_B2A', 'OP_D2A']
 
