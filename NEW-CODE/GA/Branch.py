135b8ab29b7284890028735f3d5ce6300b22d646
CrystalShrimp <shuman0805@163.com>
2024-12-12 20:35:59 +0800

    Update Branch.py

--- NEW-CODE/GA/Branch.py
+++ NEW-CODE/GA/Branch.py
@@ -8,8 +8,9 @@ sys.path.append('..')
 
 from OP import *
 from Tools.GA_tools import *
-from .Root import *
-
+## 暂时解决root中没有日频的定义
+DP_Root = []
+DV_Root = []
 class Branch:
     def __init__(self, mp_root: MP_Root, population_size: int):
         self.population_size = population_size
@@ -34,52 +35,54 @@ class Branch:
 
 
 class M_Branch_MP2D(Branch):
-    def __init__(self, mp_root: MP_Root, population_size: int):
+    def __init__(self, mp_root: MP_Root, population_size=10):
         super().__init__(mp_root, population_size)
         self.input = mp_root
 
     def add_primitive(self):
         self.pset = gp.PrimitiveSetTyped("MAIN",[TypeB]*len(self.input), TypeD)
         # 添加TypeB
-        for root in self.input():
+        for root in self.input:
             self.pset.addTerminal(root,TypeB)      
         # 添加TypeF
-        for constant in self.int_values():
+        for constant in self.int_values:
             self.pset.addTerminal(constant,TypeF)
-        self.pset.addPrimitive(OP_Closure.id_int(),TypeF,TypeF)
+        self.pset.addPrimitive(OP_Closure.id_int,[TypeF],TypeF)
         # 添加primitive
-        op = OP_BF2D
+        op = OP_BF2D()
         for name in op.func_list:
             func = getattr(op, name)
             self.pset.addPrimitive(func, [TypeB,TypeF], TypeD)
-        op = OP_B2D
+        op = OP_B2D()
         for name in op.func_list:
             func = getattr(op, name)
-            self.pset.addPrimitive(func, TypeB, TypeD)
+            self.pset.addPrimitive(func, [TypeB], TypeD)
         # 创建工具箱
         super().generate_toolbox()
 
     def run(self):
         self.add_primitive()
-        self.generate.population()
+        self.generate_population()
 
 class M_Branch_MPDP2D(Branch):
     # missing DP_Root
-    def __init__(self, mp_root: MP_Root, dp_root: DP_Root,population_size: int):
+    def __init__(self, mp_root: MP_Root, dp_root: DP_Root,population_size=10):
+        # 暂时没有DP
         super().__init__(mp_root, population_size)
-        self.input1 = mp_root
-        self.input2 = dp_root
+        self.input_m = mp_root
+        self.input_d = dp_root
+        self.input = self.input_m + self.input_d
 
     def add_primitive(self):
-        self.pset = gp.PrimitiveSetTyped("MAIN",[TypeB]*len(self.input1)+[TypeA]*len(self.input2), TypeD)
+        self.pset = gp.PrimitiveSetTyped("MAIN",[TypeB]*len(self.input_m)+[TypeA]*len(self.input_d), TypeD)
         # 添加TypeB
-        for root in self.input1():
+        for root in self.input[0]:
             self.pset.addTerminal(root,TypeB)      
         # 添加TypeA
-        for root in self.input2():
+        for root in self.input[1]:
             self.pset.addTerminal(root,TypeA)      
         # 添加primitive
-        op = OP_BA2D
+        op = OP_BA2D()
         for name in op.func_list:
             func = getattr(op, name)
             self.pset.addPrimitive(func, [TypeB,TypeA], TypeD)
@@ -88,55 +91,56 @@ class M_Branch_MPDP2D(Branch):
 
     def run(self):
         self.add_primitive()
-        self.generate.population()
+        self.generate_population()
 
 class M_Branch_MV2D(Branch):
     # missing MV_Root
-    def __init__(self, mv_root: MV_Root, population_size: int):
+    def __init__(self, mv_root: MV_Root, population_size=10):
         super().__init__(mv_root, population_size)
         self.input = mv_root
     def add_primitive(self):
         self.pset = gp.PrimitiveSetTyped("MAIN",[TypeB]*len(self.input), TypeD)
         # 添加TypeB
-        for root in self.input():
+        for root in self.input:
             self.pset.addTerminal(root,TypeB)      
         # 添加TypeF
-        for constant in self.int_values():
+        for constant in self.int_values:
             self.pset.addTerminal(constant,TypeF)
-        self.pset.addPrimitive(OP_Closure.id_int(),TypeF,TypeF)
+        self.pset.addPrimitive(OP_Closure.id_int,[TypeF],TypeF)
         # 添加primitive
-        op = OP_BF2D
+        op = OP_BF2D()
         for name in op.func_list:
             func = getattr(op, name)
             self.pset.addPrimitive(func, [TypeB,TypeF], TypeD)
-        op = OP_B2D
+        op = OP_B2D()
         for name in op.func_list:
             func = getattr(op, name)
-            self.pset.addPrimitive(func, TypeB, TypeD)
+            self.pset.addPrimitive(func, [TypeB], TypeD)
         # 创建工具箱
         super().generate_toolbox()
 
     def run(self):
         self.add_primitive()
-        self.generate.population()
+        self.generate_population()
 
 class M_Branch_MVDV2D(Branch):
-    # missing MV_Root, DV_Root
-    def __init__(self, mv_root: MV_Root, dv_root: DV_Root,population_size: int):
+    # 暂时没有DV
+    def __init__(self, mv_root: MV_Root, dv_root: DV_Root,population_size=10):
         super().__init__(mv_root, population_size)
-        self.input1 = mv_root
-        self.input2 = dv_root
+        self.input_m = mv_root
+        self.input_d = dv_root
+        self.input = self.input_m + self.input_d
 
     def add_primitive(self):
-        self.pset = gp.PrimitiveSetTyped("MAIN",[TypeB]*len(self.input1)+[TypeA]*len(self.input2), TypeD)
+        self.pset = gp.PrimitiveSetTyped("MAIN",[TypeB]*len(self.input_m)+[TypeA]*len(self.input_d), TypeD)
         # 添加TypeB
-        for root in self.input1():
+        for root in self.input_m:
             self.pset.addTerminal(root,TypeB)      
         # 添加TypeA
-        for root in self.input2():
+        for root in self.input_d:
             self.pset.addTerminal(root,TypeA)      
         # 添加primitive
-        op = OP_BA2D
+        op = OP_BA2D()
         for name in op.func_list:
             func = getattr(op, name)
             self.pset.addPrimitive(func, [TypeB,TypeA], TypeD)
@@ -145,4 +149,21 @@ class M_Branch_MVDV2D(Branch):
 
     def run(self):
         self.add_primitive()
-        self.generate.population()
+        self.generate_population()
+
+if __name__ == "__main__":
+    mp_root = ['M_cs_scale(M_ts_mean_left_neighbor(M_O, 5, -1))', 'M_ts_pctchg(M_ts_mean_right_neighbor(M_C, 10, 1))', 'M_cs_zscore(M_ts_mean_left_neighbor(M_O, 5, -1))', 'M_cs_rank(M_ts_mean_left_neighbor(M_O, 5, -1))']
+    dp_root = ['D_ts_pctchg(D_at_abs(D_O))','D_ts_pctchg(D_ts_max(D_O,5))']
+    m_branch_mp2d = M_Branch_MP2D(mp_root)
+    m_branch_mp2d.run()
+    print("M_Branch_MP2D Individual Str: ", m_branch_mp2d.individuals_str)
+    print("M_Branch_MP2D Individual Code: ", m_branch_mp2d.individuals_code)
+
+    m_branch_mpdp2d = M_Branch_MPDP2D(mp_root,dp_root)
+
+    m_branch_mpdp2d.generate_toolbox()
+    print(type(m_branch_mpdp2d.population_size))
+    m_branch_mpdp2d.run()
+
+    print("M_Branch_MP2D Individual Str: ", m_branch_mpdp2d.individuals_str)
+    print("M_Branch_MP2D Individual Code: ", m_branch_mpdp2d.individuals_code)
