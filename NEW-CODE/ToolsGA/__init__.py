99309dcab368c158ad8c975a69478b5c6530ba86
Orville-Yan <13396864550@163.com>
2025-01-13 12:14:52 +0800

    Delete NEW-CODE directory

--- NEW-CODE/ToolsGA/__init__.py
+++
@@ -1,9 +0,0 @@
-from .BackTest import stratified, backtest
-from .DataReader import DataReader
-from .GA_tools import TypeA, TypeB, TypeC, TypeD, TypeE, TypeF, TypeG, change_name
-
-__all__ = [
-    'stratified', 'backtest',
-    'DataReader',
-    'TypeA', 'TypeB', 'TypeC', 'TypeD', 'TypeE', 'TypeF', 'TypeG', 'change_name'
-]
