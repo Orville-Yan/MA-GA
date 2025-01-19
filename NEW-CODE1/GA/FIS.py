a88020b098d9e664be6ea87b9b51168eea5a6c4a
CrystalShrimp <shuman0805@163.com>
2025-01-19 21:03:28 +0800

    Update FIS.py

--- NEW-CODE1/GA/FIS.py
+++ NEW-CODE1/GA/FIS.py
@@ -5,96 +5,99 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.append(parent_dir_path)
 
-from deap import creator, base, tools, gp
-import operator
-import random
 from deap import gp
 import networkx as nx
+import pandas as pd
 from itertools import combinations
-from RPN.RPNbuilder import *
+from RPN.RPNbuilder import*
 import difflib
-# 调了一个用了算字符串相似度的包difflib用来算边
 class BK_Algo: #Bron-Kerbosch Algo
-    # 直接调用networkx库内置的最大团算法？也可以调出源码放在这里。
     pass
 
 
 class FactorIntoStorage(RPN_Compiler):
     def __init__(self,new_factor:list):
         super().__init__()
-        self.storage_path=None
-        self.new_factor=[gp.PrimitiveTree.from_string(RPN, self.pset) for RPN in new_factor]
+        self.storage_path = r'D:\运行文档\NFE遗传算法项目\NEW-CODE1'
+        self.new_factor = new_factor
 
     def get_exist_factor(self):
-        existing_factors = []
         if self.storage_path:  
-            with open(self.storage_path, 'r', encoding='utf-8') as file:
-                for line in file:
-                    existing_factors.append(gp.PrimitiveTree.from_string(line.strip(), self.pset))
+            file_path = os.path.join(self.storage_path,'factor_storage.xlsx')
+            file = pd.read_excel(file_path)
+            existing_factors = list(file['tree'])
+        else:
+            raise ValueError("存储路径未定义")
         self.exist_factor = existing_factors 
 
     def greedy_algo(self):
         selected_factors = []
+        exist_factor = self.exist_factor
         for new_factor in self.new_factor:
             should_add = True
-            for exist_factor in self.exist_factor:
-                similarity = self.calculate_similarity(new_factor, exist_factor)
-                if similarity >= 0.9:  
+            for factor in exist_factor:
+                similarity = self.calculate_similarity(new_factor, factor)
+                if similarity >= 0.6:  
                     should_add = False
                     break
             if should_add:
                 selected_factors.append(new_factor)
+                exist_factor.append(new_factor)
         self.greedy_factor = selected_factors
     @staticmethod
-   
-    def calculate_similarity(factor1, factor2):
-        matcher = difflib.SequenceMatcher(None,str(factor1),str(factor2))
+    def calculate_similarity(factor1:gp.PrimitiveTree, factor2:gp.PrimitiveTree):
+        matcher = difflib.SequenceMatcher(None,factor1,factor2)
         return  matcher.ratio()
-    def build_factor_graph(self,old_strings:str, new_strings:str):
+    def build_factor_graph(self,factors_lst:list):
         graph = nx.Graph()
-        all_strings = old_strings + new_strings
-        graph.add_nodes_from(all_strings)
-        for factor1, factor2 in combinations(all_strings, 2):
+        graph.add_nodes_from(factors_lst)
+        for factor1, factor2 in combinations(factors_lst, 2):
             similarity = self.calculate_similarity(factor1, factor2)
-            if similarity >= 0.6: 
+            if similarity < 0.6: 
                 graph.add_edge(factor1, factor2, weight=similarity)
         return graph
 
     def bk_algo(self):
-        old_strings = [str(factor) for factor in self.exist_factor]
-        new_strings = [str(factor) for factor in self.new_factor]
-        graph = self.build_factor_graph(old_strings, new_strings)
-        # nx内置的极大团算法，有源码可以添加
+        new_factors = self.new_factor
+        graph = self.build_factor_graph(new_factors)
         maximal_cliques = list(nx.find_cliques(graph))
         best_clique = max(maximal_cliques, key=len)
-        selected_strings = [factor for factor in best_clique if factor in new_strings]
+        selected_strings = [factor for factor in best_clique if factor in new_factors]
         self.bk_factor = selected_strings
     def factor_evaluating(self):
         pass
 
     def store_factors(self):
         self.add_factor = self.bk_factor
+        # factor_storage = pd.DataFrame(columns = ['tree','in_sample','out_sample','annual_yield','trunk','root','seed','branch','subtree'])
         if self.storage_path:
-            with open(self.storage_path, 'a', encoding='utf-8') as file:
-                for factor in self.add_factor:
-                    file.write(factor + '\n')
+            file_path = os.path.join(self.storage_path,'factor_storage.xlsx')
+            factor_storage = pd.read_excel(file_path)
         else:
             raise ValueError("存储路径未定义")
+        def tree2lst(tree):
+            if type(tree) == list:
+                return [parser.tree2str(x) for x in tree]
+            else:
+                return parser.tree2str(tree)
+            
+        for factor in self.add_factor:
+            parser = RPN_Parser(factor)
+            parser.get_tree_structure()
+            parser.parse_tree()
+            tree = parser.tree2str(parser.tree['tree_mode'])
+            trunks = tree2lst(parser.trunk['tree_mode'])
+            roots = tree2lst(parser.root['tree_mode'])
+            seeds = tree2lst(parser.seed['tree_mode'])
+            branches = tree2lst(parser.branch['tree_mode'])
+            subtrees = tree2lst(parser.subtree['tree_mode'])
+            factor_storage.loc[factor_storage.shape[0]+1] = {'tree':tree, 'trunk':trunks,'root':roots,'seed':seeds,'branch':branches,'subtree':subtrees}
+        factor_storage.to_excel(file_path)
 if __name__ == "__main__":
-    from deap import creator,base,tools
-    pset = gp.PrimitiveSet("MAIN", arity=2) 
-    pset.addPrimitive(operator.add, 2)
-    pset.addPrimitive(operator.sub, 2)  
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-    creator.create('Individual',gp.PrimitiveTree,fitness = creator.FitnessMin)
-    toolbox = base.Toolbox()
-    toolbox.register('expr',gp.genHalfAndHalf,pset=pset, min_=4, max_=4)
-    toolbox.register('Individual',tools.initIterate,creator.Individual,toolbox.expr)
-    toolbox.register('population',tools.initRepeat,list,toolbox.Individual)
-    code = toolbox.population(20)
-    FIS = FactorIntoStorage([''])
-    FIS.new_factor = code[:10]
-    FIS.exist_factor = code[10:]
+    producer = RPN_Producer()
+    producer.run()
+    FIS = FactorIntoStorage(producer.tree)
+    FIS.get_exist_factor()
     FIS.bk_algo()
-    FIS.greedy_algo()
-    print(FIS.bk_factor)
+    FIS.store_factors()
+
