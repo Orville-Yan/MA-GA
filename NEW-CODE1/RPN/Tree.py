ef7d977264ba711be5ea87599d68f0e75af1705e
CrystalShrimp <shuman0805@163.com>
2025-02-11 19:14:57 +0800

    Update Tree.py

--- NEW-CODE1/RPN/Tree.py
+++ NEW-CODE1/RPN/Tree.py
@@ -1,5 +1,8 @@
 import sys
-sys.path.append('..')
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.append(parent_dir_path)
 
 from ToolsGA.GA_tools import *
 from OP import *
