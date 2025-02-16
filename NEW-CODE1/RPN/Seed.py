70b0a74c9fb7e5f12c5926b5c1715aab2791cf0c
CrystalShrimp <shuman0805@163.com>
2025-02-16 12:36:15 +0800

    Update Seed.py

--- NEW-CODE1/RPN/Seed.py
+++ NEW-CODE1/RPN/Seed.py
@@ -6,7 +6,7 @@ sys.path.append(parent_dir_path)
 
 from ToolsGA.GA_tools import *
 from OP import *
-from Config import Seed_Config as Config
+from GA.Config import Seed_Config as Config
 
 
 class Seed:
